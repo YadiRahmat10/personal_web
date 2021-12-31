7f00100bb6150d6a8ee16fca9f894a5d668381dc
Yadirahmat10 <yaddrahmat@gmail.com>
2021-12-31 10:37:46 +0700

    personalWeb

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const db = require('./connection/db');
 const upload = require('./middleware/uploadFile');
 
 const app = express();
+// const PORT = 3000;
 const PORT = process.env.PORT
 
 function getFullTime(t) {
