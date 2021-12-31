08054b97d9dd0da21062f22926eaaa57938c0857
Yadirahmat10 <yaddrahmat@gmail.com>
2021-12-31 08:13:07 +0700

    personal_web

--- connection/db.js
+++ connection/db.js
@@ -1,10 +1,15 @@
 const { Pool } = require('pg');
 
 const dbPool = new Pool({
-  database: 'personal_web_b29',
-  port: 5432,
-  user: 'postgres',
-  password: 'root',
+  // database: 'personal_web_b29',
+  // port: 5432,
+  // user: 'postgres',
+  // password: 'root',
+  connectionString: 'postgres://occhzuepicagfk:d29237073756dc9e0fd4444edfb7eae578712c24962efb467f5d6f539adf0c55@ec2-18-210-118-224.compute-1.amazonaws.com:5432/d7b34dusgrqdvc',
+  ssl: {
+     rejectUnauthorized: false,
+  },
 });
 
 module.exports = dbPool;
+
